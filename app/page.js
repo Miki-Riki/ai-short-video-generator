91ef58e44118fd5e4211a192a1ae3988838bc55f
Miki-Riki <david.12.simeg@gmail.com>
2025-01-31 21:12:31 +0100

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client";
+/* "use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -23,3 +23,33 @@ export default function Home() {
   );
 }
 
+ */
+
+
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { UserButton, useUser } from "@clerk/nextjs";
+
+export default function Home() {
+  const router = useRouter();
+  const { isSignedIn, isLoading } = useUser();
+
+  useEffect(() => {
+    // Ensure we're not redirecting before the user's status is loaded
+    if (!isLoading) {
+      if (isSignedIn) {
+        router.push("/dashboard");
+      } else {
+        router.push("/sign-in");
+      }
+    }
+  }, [isSignedIn, isLoading, router]);
+
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
