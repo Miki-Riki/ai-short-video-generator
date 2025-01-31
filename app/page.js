6e793c67402e4ece79323c44473ec3215a4b84f0
Miki-Riki <david.12.simeg@gmail.com>
2025-01-31 21:26:58 +0100

    changes

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-/* "use client";
+"use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -13,34 +13,6 @@ export default function Home() {
       router.push("/dashboard");
     } else {
       router.push("/sign-in");
-    }
-  }, [isSignedIn, router]);
-
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
-}
-
- */
-
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { UserButton, useUser } from "@clerk/nextjs";
-
-export default function Home() {
-  const router = useRouter();
-  const { isSignedIn } = useUser();
-
-  useEffect(() => {
-    // If the user is signed out, force a page reload and redirect to sign-in
-    if (!isSignedIn) {
-      window.location.href = "/sign-in";  // This will reload the page and redirect to sign-in
-    } else {
-      router.push("/dashboard");
     }
   }, [isSignedIn, router]);
 
