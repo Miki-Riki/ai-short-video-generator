719797c9a96c36e53ea0fa3300db965087f66a5c
Miki-Riki <david.12.simeg@gmail.com>
2025-01-31 21:15:24 +0100

    finals

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-/* "use client";
+"use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -15,40 +15,6 @@ export default function Home() {
       router.push("/sign-in");
     }
   }, [isSignedIn, router]);
-
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
-}
-
- */
-
-
-"use client";
-
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { UserButton, useUser } from "@clerk/nextjs";
-
-export default function Home() {
-  const router = useRouter();
-  const { isSignedIn, isLoading } = useUser();
-  const [initialLoad, setInitialLoad] = useState(true);
-
-  useEffect(() => {
-    // Check if the user session has changed and we're not loading
-    if (!isLoading) {
-      if (initialLoad) {
-        setInitialLoad(false); // Mark initial load as complete
-      } else if (isSignedIn) {
-        router.push("/dashboard");
-      } else {
-        router.push("/sign-in");
-      }
-    }
-  }, [isSignedIn, isLoading, router, initialLoad]);
 
   return (
     <div>
