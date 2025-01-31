ed0602d3fe852c9b743dffcd99e0c112c5b9e5fc
Miki-Riki <david.12.simeg@gmail.com>
2025-01-31 21:18:11 +0100

    page.js fixes

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client";
+/* "use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -23,3 +23,37 @@ export default function Home() {
   );
 }
 
+ */
+
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { UserButton, useUser } from "@clerk/nextjs";
+
+export default function Home() {
+  const router = useRouter();
+  const { isSignedIn, isLoading } = useUser();
+  const [redirecting, setRedirecting] = useState(true);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setRedirecting(false);
+      if (isSignedIn) {
+        router.push("/dashboard");
+      } else {
+        router.push("/sign-in");
+      }
+    }
+  }, [isSignedIn, isLoading, router]);
+
+  if (redirecting || isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
