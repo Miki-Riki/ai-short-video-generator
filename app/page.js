9cccfbfb21b4d2ddff8f273962f00318394b3b7e
Miki-Riki <david.12.simeg@gmail.com>
2025-02-10 22:10:03 +0100

    final touches

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-/* "use client";
+"use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -19,30 +19,6 @@ export default function Home() {
   return (
     <div>
       <UserButton />
-    </div>
-  );
-} */
-
-  "use client";
-
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useUser } from "@clerk/nextjs";
-
-export default function Home() {
-  const router = useRouter();
-  const { isSignedIn } = useUser();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (isSignedIn === undefined) return; // Wait until isSignedIn is determined
-    setLoading(false);
-    router.push(isSignedIn ? "/dashboard" : "/sign-in");
-  }, [isSignedIn, router]);
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      {loading && <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>}
     </div>
   );
 }
